5c5d54e75f10674226fc2b69ab7ad235b289d3fe
jeonguk29 <jeonguk29@naver.com>
2023-07-31 17:15:01 +0900

    SwiftUI Study Start

---
+++ SwiftUIBasic/SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by 정정욱 on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
