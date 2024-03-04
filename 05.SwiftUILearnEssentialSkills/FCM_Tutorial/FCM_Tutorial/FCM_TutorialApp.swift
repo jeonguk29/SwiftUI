662a21f5a197e4ad9a5290448a5c635f29efc287
jeonguk29 <jeonguk29@naver.com>
2024-03-04 23:21:02 +0900

    FCM 서버 푸시 받기

--- 05.SwiftUILearnEssentialSkills/FCM_Tutorial/FCM_Tutorial/FCM_TutorialApp.swift
+++ 05.SwiftUILearnEssentialSkills/FCM_Tutorial/FCM_Tutorial/FCM_TutorialApp.swift
@@ -6,12 +6,103 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseMessaging
 
 @main
 struct FCM_TutorialApp: App {
+    
+    //SwiftUI에서 만든 AppDelegate 사용방법
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+/*
+ SwiftUI에서 FCM 사용시 AppDelegate가 없어서 만들어줘야함
+ */
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    // 앱이 켜졌을때
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // Use Firebase library to configure APIs
+        // 파이어베이스 설정
+        FirebaseApp.configure()
+        
+        // 원격 알림 등록 (공식문서에서 하라는데로)
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+          )
+        } else {
+          let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+        // UNUserNotificationCenterDelegate가 필요해서 확장으로 관리
+        application.registerForRemoteNotifications()
+        
+        // 메세징 델리겟 MessagingDelegate가 필요해서 확장으로 관리
+        Messaging.messaging().delegate = self
+        
+        
+        // 푸시 포그라운드 설정 (앱이 열려있을때도 알림 받을 수 있도록)
+        UNUserNotificationCenter.current().delegate = self
+        
+        return true
+    }
+    
+    // fcm 토큰이 등록 되었을 때
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+}
+
+extension AppDelegate : MessagingDelegate {
+    
+    // fcm 등록 토큰을 받았을 때
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("AppDelegate - 파베 토큰을 받았다.")
+        print("AppDelegate - Firebase registration token: \(String(describing: fcmToken))")
+    }
+}
+
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    // 푸시메세지가 앱이 켜져 있을때 나올때
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+       
+        // notification으로 정보가 들어옴
+        let userInfo = notification.request.content.userInfo
+        
+        print("willPresent: userInfo: ", userInfo)
+        
+        completionHandler([.banner, .sound, .badge])// 배너, 소리, 뱃지에 표시
+    }
+    
+    // 푸시메세지를 받았을 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        print("didReceive: userInfo: ", userInfo)
+        completionHandler()
+    }
+    
 }
